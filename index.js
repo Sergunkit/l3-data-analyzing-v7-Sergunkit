415e286221cb09c0d5b56c26b0a49c385cfa6831
Sergunkit <sergunkit@mail.ru>
2023-05-31 13:19:50 +0300

    4 commit

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-// import content from './bin/cities.js'
+import _ from 'lodash';
 
 export default function solution(content) {
-    const data = content.split('\n')
-     .slice(1)
-     .map((str) => (str).split('|'))
-     .map((str) => str.map((el) => el.trim()))
-     .map((el) => el.filter((elt) => (elt)));
-     console.log(`Число записей: ${data.length}`);
-     const ters = data.map((el) => el[4]).sort()
-     console.log(`Список округов: ${ters}`);
-     const sqrs = data.map((el) => (el[2]))
-    //  const numSqr = sqrs.map((el) => Number(el))
-    for (const sqr in sqrs) {
-        
-    }
-     console.log(numSqr)
-    //  console.log(`Наибольшая площадь: ${Math.max(sqrs)} кв.км.`)
+  const data = content.split('\n')
+    .slice(1)
+    .map((str) => (str).split('|'))
+    .map((str) => str.map((el) => el.trim()))
+    .map((el) => el.filter((elt) => (elt)));
+  console.log(`Число записей: ${data.length}`);
+  let ters = data.map((el) => el[4]).sort();
+  ters = ters.map((el) => ` ${el}`);
 
+  console.log(`Список округов:${_.union(ters)}`);
+  const sqrs = data.map((el) => (el[2]));
+  const numSqr = sqrs.map((el) => Number(el));
 
+  console.log(numSqr);
+  console.log(`Наибольшая площадь: ${Math.max(sqrs)} кв.км.`);
 
-    // console.log(data);
+  // console.log(data);
 }
