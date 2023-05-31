7ac5346b7d9d00c231a9e6d349919486f5299fb0
Sergunkit <sergunkit@mail.ru>
2023-05-31 12:53:56 +0300

    3 commit

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ export default function solution(content) {
      .map((str) => (str).split('|'))
      .map((str) => str.map((el) => el.trim()))
      .map((el) => el.filter((elt) => (elt)));
-     console.log(`'Число записей: ${data.length}'`);
+     console.log(`Число записей: ${data.length}`);
      const ters = data.map((el) => el[4]).sort()
-     console.log(`'Список округов: ${ters}'`);
+     console.log(`Список округов: ${ters}`);
+     const sqrs = data.map((el) => (el[2]))
+    //  const numSqr = sqrs.map((el) => Number(el))
+    for (const sqr in sqrs) {
+        
+    }
+     console.log(numSqr)
+    //  console.log(`Наибольшая площадь: ${Math.max(sqrs)} кв.км.`)
 
 
 
