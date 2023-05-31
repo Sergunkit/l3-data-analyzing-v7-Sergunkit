01cacf032197e4e95d44341e32230cb7249173ad
Sergunkit <sergunkit@mail.ru>
2023-05-31 12:28:47 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// import content from './bin/cities.js'
 
+export default function solution(content) {
+    const data = content.split('\n')
+     .slice(1)
+     .map((str) => (str).split('|'))
+     .map((str) => str.map((el) => el.trim()))
+     .map((el) => el.filter((elt) => (elt)));
+     console.log(data.length);
+     const ters = data.map((el) => el[4]).sort()
+     console.log(ters);
+     
+
+
+    // console.log(data);
+}
