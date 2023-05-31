74f4161c9982a039db6d8775ee935e11fa11fe29
Sergunkit <sergunkit@mail.ru>
2023-05-31 16:36:30 +0300

    last commit

--- index.js
+++ index.js
@@ -11,11 +11,36 @@ export default function solution(content) {
   ters = ters.map((el) => ` ${el}`);
 
   console.log(`Список округов:${_.union(ters)}`);
-  const sqrs = data.map((el) => (el[2]));
-  const numSqr = sqrs.map((el) => Number(el));
-
-  console.log(numSqr);
-  console.log(`Наибольшая площадь: ${Math.max(sqrs)} кв.км.`);
-
-  // console.log(data);
+  const sqrs = data.map((el) => el[2]);
+  const numSqr = sqrs.map((elt) => {
+    const ter = Number(elt.replace(/ /g, ''));
+    return ter;
+  });
+  console.log(`Наибольшая площадь: ${Math.max(...numSqr)} кв.км.`);
+//   console.log(data);
+  const resps = data.filter((el) => {
+    const resp = el[0].split(' ');
+    return resp[0] === 'Республика';
+  });
+  const population = resps.map((el) => el[1]);
+  const numPop = population.map((elt) => {
+    const pop = Number(elt.replace(/ /g, ''));
+    return pop;
+  });
+  const maxPop = Math.max(...numPop);
+  const maxPopResp = resps.filter((el) => Number(el[1].replace(/ /g, '')) === maxPop);
+  console.log(`Республика с наибольшим количеством населения: ${maxPopResp[0][0]}`);
+  const densitys = data.reduce((acc, el) => {
+    const pop = Number(el[1].replace(/ /g, ''));
+    const ter = Number(el[2].replace(/ /g, ''));
+    const density = pop / ter;
+    if (!acc.name) {
+      return { name: el[0], density };
+    }
+    if ((acc.density) > density) {
+      return { ...acc, name: el[0], density };
+    }
+    return acc;
+  }, {});
+  console.log(`Наименьшая плотность населения: ${densitys.name}`);
 }
